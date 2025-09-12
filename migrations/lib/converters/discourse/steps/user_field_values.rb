discourse/discourse

DEV: Add support for converting and importing linked `user_custom_fields`
First pass at converting and importing user customer fields linked to
user fields

migrations/lib/converters/discourse/steps/user_field_values.rb
@@ -1,0 +1,38 @@
+# frozen_string_literal: true
+
+module Migrations::Converters::Discourse
+  class UserFieldValues < ::Migrations::Converters::Base::ProgressStep
+    USER_FIELD_PREFIX = "user_field_"
+
+    attr_accessor :source_db
+
+    def max_progress
+      @source_db.count <<~SQL
+        SELECT COUNT(*)
+        FROM user_custom_fields
+        WHERE user_id >= 0
+          AND name LIKE '#{USER_FIELD_PREFIX}%'
+      SQL
+    end
+
+    def items
+      @source_db.query <<~SQL
+        SELECT user_custom_fields.*,
+               CAST(REPLACE(name, '#{USER_FIELD_PREFIX}', '') AS NUMERIC) AS field_id
+        FROM user_custom_fields
+        WHERE user_id >= 0
+          AND name LIKE '#{USER_FIELD_PREFIX}%'
+        ORDER BY user_id
+      SQL
+    end
+
+    def process_item(item)
+      IntermediateDB::UserFieldValue.create(
+        created_at: item[:created_at],
+        field_id: item[:field_id].to_i,
+        user_id: item[:user_id],
+        value: item[:value],
+      )
+    end
+  end
+end

ai-code-review-benchmarks/greptile-v3-discourse

migrations/lib/converters/discourse/steps/user_field_values.rb
@@ -1,0 +1,38 @@
+# frozen_string_literal: true
+
+module Migrations::Converters::Discourse
+  class UserFieldValues < ::Migrations::Converters::Base::ProgressStep
+    USER_FIELD_PREFIX = "user_field_"
+
+    attr_accessor :source_db
+
+    def max_progress
+      @source_db.count <<~SQL
+        SELECT COUNT(*)
+        FROM user_custom_fields
+        WHERE user_id >= 0
+          AND name LIKE '#{USER_FIELD_PREFIX}%'
+      SQL
+    end
+
+    def items
+      @source_db.query <<~SQL
+        SELECT user_custom_fields.*,
+               CAST(REPLACE(name, '#{USER_FIELD_PREFIX}', '') AS INTEGER) AS field_id
+        FROM user_custom_fields
+        WHERE user_id >= 0
+          AND name LIKE '#{USER_FIELD_PREFIX}%'
+        ORDER BY user_id
+      SQL
+    end
+
+    def process_item(item)
+      IntermediateDB::UserFieldValue.create(
+        created_at: item[:created_at],
+        field_id: item[:field_id],
+        user_id: item[:user_id],
+        value: item[:value],
+      )
+    end
+  end
+end